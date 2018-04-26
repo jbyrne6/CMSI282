fef9d94e537d082bb281d6970cd5613758efbade
jbyrne6 <jbyrne5094@gmail.com>
2018-04-25 23:46:17 -0700

    Add files via upload

--- Toal/piano_keys.c
+++ Toal/piano_keys.c
@@ -5,15 +5,12 @@
 int main() {
   char *KEY_NAMES[] = {"A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"};
   int STEPS = sizeof( KEY_NAMES ) / sizeof( KEY_NAMES[0] );
-  int INITIAL_FREQUENCY = 27.5;
+  float INITIAL_FREQUENCY = 27.5;
   int NUMBER_OF_KEYS = 88;
 
   for (int i = 0; i < NUMBER_OF_KEYS; i++) {
     char *key_name = KEY_NAMES[i % STEPS];
     float frequency = INITIAL_FREQUENCY * ( pow( 2.0, (float)i / (float)STEPS ) ) ;
-    //printf("i:  %d\n", i);
-    //printf("STEPS:  %d\n", STEPS);
-    //printf("i/STEPS:  %10.4f\n", ((float)i / (float)STEPS));
     printf("%s    %10.4f\n", key_name, frequency);
   }
   return 0;
